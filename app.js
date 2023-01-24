279f66ae39982a56f530c0041128cdc6243ca09a
Thiago <thiagodelgado.dev@gmail.com>
2023-01-24 11:53:10 -0300

    add view mobile done

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require("path");
 const app = express();
 const port = 3030;
 
-app.use(express.static(path.resolve(__dirname,"public")));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"views","home.html")));
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
 
 app.listen(port, () => console.log(`Server Running in: http://localhost:${port}`));
